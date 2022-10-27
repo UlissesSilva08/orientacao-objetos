3eb120a2cd87e146e58a5cd4d084f72e098f2256
ALUNO - SL103 <sl103@escolasebraesp.local>
2022-10-26 22:22:46 -0300

    segundo commit e adição do login

--- class.php
+++ class.php
@@ -1,47 +1,86 @@
 <?php 
-    class Pessoa{
-        public $nome;
-        public $idade;
-        public $peso;
+    // class Pessoa{
+    //     public $nome;
+    //     public $idade;
+    //     public $peso;
 
-        public function andar(){
-            echo "andar andar"; 
+    //     public function andar(){
+    //         echo "andar andar"; 
+    //     }
+
+    //     public function falar(){
+    //         echo "bla bla bla";
+    //     }
+
+    //     public function imprimirDados(){
+    //         echo $this->nome;
+    //     }
+    // }
+
+    // class Carro{
+    //     public $marca;
+    //     public $modelo;
+    //     public $ano;
+    //     public $cor;
+    //     public $motor;
+    //     public $cv;
+
+    //     public function dadosCarro(){
+    //         echo "Marca: ".$this->marca."<br>";
+    //         echo "Modelo: ".$this->modelo."<br>";
+    //         echo "Ano: ".$this->ano."<br>";
+    //         echo "Cor: ".$this->cor."<br>";
+    //         echo "Motor: ".$this->motor."<br>";
+    //         echo "Potência: ".$this->cv."<br>";
+    //     }
+    // }
+
+    class Login{
+        private $email;
+        private $senha;
+
+        public function setEmail($e){
+            $this -> email = $e;
         }
 
-        public function falar(){
-            echo "bla bla bla";
+        public function getEmail(){
+            return $this -> email;
         }
 
-        public function imprimirDados(){
-            echo $this->nome;
+        public function setSenha($s){
+            $this -> senha = $s;
+        }
+
+        public function getSenha(){
+            return $this -> senha;
+        }
+
+        public function login(){
+            if($this -> email == "teste@teste.com" && $this -> senha == "123"){
+                echo "Logado com sucesso";
+            }
+            else{
+                echo "Não logado";
+            }
         }
     }
 
-    class Carro{
-        public $marca;
-        public $modelo;
-        public $ano;
-        public $cor;
-        public $motor;
-        public $cv;
+    $logged = new Login();
+    $logged -> setEmail("teste@teste.com");
+    $logged -> setSenha("123");
 
-        public function dadosCarro(){
-            echo "Marca: ".$this->marca."<br>";
-            echo "Modelo: ".$this->modelo."<br>";
-            echo "Ano: ".$this->ano."<br>";
-            echo "Cor: ".$this->cor."<br>";
-            echo "Motor: ".$this->motor."<br>";
-            echo "Potência: ".$this->cv."<br>";
-        }
-    }
+    echo($logged -> getEmail(). "<br>");
+    echo($logged -> getSenha(). "<br>");
 
-    $aventador = new Carro();
-    $aventador -> marca = "Lamborghini";
-    $aventador -> modelo = "Aventador LP-700";
-    $aventador -> ano = "2012";
-    $aventador -> cor = "Laranja";
-    $aventador -> motor = "V12 6.5l biTurbo";
-    $aventador -> cv = "700cv";
+    $logged -> login();
 
-    $aventador ->dadosCarro();
+    // $aventador = new Carro();
+    // $aventador -> marca = "Lamborghini";
+    // $aventador -> modelo = "Aventador LP-700";
+    // $aventador -> ano = "2012";
+    // $aventador -> cor = "Laranja";
+    // $aventador -> motor = "V12 6.5l biTurbo";
+    // $aventador -> cv = "700cv";
+
+    // $aventador ->dadosCarro();
 ?>
